bae41e2b346825db1adc31d300e852195c39440f
Wallisson Martins <wallissonmartins37@gmail.com>
2022-12-19 10:31:57 -0300

    edit msg

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,34 @@ interface IClickedPoints {
 }
 
 function App() {
-  
   const [clickedPoints, setClickedPoints] = useState<IClickedPoints[]>([]);
   const [restorePoints, setRestorePoints] = useState<IClickedPoints[]>([]);
 
   const phrases: string[] = [
-    'Você',
-    'é',
-    'incrível',
-    'e',
-    'merece',
-    'todo',
-    'respeito',
-    'do',
-    'mundo!',
-    'Desculpa',
-    'se',
-    'de',
-    'alguma',
-    'forma',
-    'te',
-    'magoei!',
+    'Tenha coragem para se tornar aquilo que sonha.',
+    'Os sonhos servem para abrir caminho e mostrar a direção.',
+    'A vida é uma aventura ousada ou nada...',
+    'Tenha orgulho da pessoa que você é.',
+    'Faça hoje o que vai te dar orgulho amanhã.',
+    'Perdoe a si pelos seus erros. Não se torture por bobagem',
+    'Grandes passos requerem coragem. Ainda bem que você é corajoso!',
+    'A persistência realiza o impossível.',
+    'Permita-se ser feliz.',
+    'Embarque numa aventura pessoal: descubra-se.',
+    'Ser uma pessoa melhor é o objetivo do dia',
+    'O futuro ainda não chegou, seja grato pelo agora.',
+    'Seu maior medo também pode ser sua maior motivação.',
+    'A força de vontade é meu superpoder',
+    'Você é forte, você é capaz, você pode',
+    'O melhor está por vir. Acredite!',
   ];
 
-  //const randomIndex = Math.floor(Math.random() * phrases.length);
+  const randomIndex = Math.floor(Math.random() * phrases.length);
 
   function showClickLocation(e: React.MouseEvent<HTMLElement>) {
     const { clientX, clientY } = e;
-    const message = phrases[clickedPoints.length];
+    //const message = phrases[clickedPoints.length];
+    const message = phrases[randomIndex];
     setClickedPoints([...clickedPoints, { clientX, clientY, message }]);
   }
 
